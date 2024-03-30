060d2fb8e9b08cbb4bf9e302b62531daccc52648
Larissa Bron <67604534+larissaissabron@users.noreply.github.com>
2024-03-29 18:49:27 -0700

    Add files via upload

--- 2024.03.28_RScript.R
+++ 2024.03.28_RScript.R
@@ -11,6 +11,7 @@ library(tidyverse)
 library(ggpubr)
 library(PerformanceAnalytics)
 library(car)
+library(gridExtra)
 
 # 1b. Confirm working directory correct
 print(getwd())
@@ -306,9 +307,7 @@ figure_5
 # 2f.  How correlated are these variables?
 chart.Correlation(coyote_data[c("pipeline", "road_gravel_1l", "trail", "conventional_seismic", "vegetated_edge_roads", "wolf_tot_det", "deer_tot_det", "moose_tot_det", "coy_tot_det", "hare_tot_det", "coy_prop_abs", "coy_prop_pres", "wolf_prop_pres", "wolf_prop_abs", "deer_prop_abs", "deer_prop_pres", "moose_prop_pres", "moose_prop_abs")], 
                   histogram = TRUE, 
-                  method = "pearson")
-
-# Interpreting correlation matrix (http://www.sthda.com/english/wiki/wiki.php?id_contents=7786)
+                  method = "pearson") # pearson is wrong because it asseses linear relationships and we don't have that, lol
 
 # --- Notes of things you would like to try still: 
 #2g. Rethink how you are doing coyote prop, could you compare presence vs. absence?
@@ -318,6 +317,133 @@ chart.Correlation(coyote_data[c("pipeline", "road_gravel_1l", "trail", "conventi
 
 # Motoring forward in the name of avoiding a rabbit hole
 
-# 3. Generalized linear models --------------------------------------------
+# 3. Generalized linear models, Poisson for count data --------------------------------------------
+
+# Anticipated models 
+# H0: Null, coyotes ~ 
+
+# H1: Coyotes like wide features, coyotes ~ pipeline + road_gravel_1l
+
+# H2: Coyote use of wide features depends on the presence of wolf competitors, coyote ~ pipeline + road_gravel_1l + pipeline:wolf_tot_det + road_gravel_1l:wolf_tot_det
+
+# H3: Coyote use of wide features varies with the presence of prey, coyote ~ pipeline + road_gravel_1l + deer_tot_det + hare_tot_det + moose_tot_det
+
+# H4: Coyotes like narrow features, coyote ~ trail + conventional_seismic + vegetated_edge_roads
+
+# H5: Coyote use of narrow features depends on presence of competitors, coyote ~ trail + conventional_seismic + vegetated_edge_roads + trail:wolf_tot_det + conventional_seismic:wolf_tot_det + vegetated_edge_roads:wolf_tot_det
+
+# H6: Coyote use of narrow features also varies with the presence of prey, coyotes ~ trail + conventional_seismic + vegetated_edge_roads + deer + hare + moose
+
+# H7: Coyotes like both wide and narrow features, coyote ~ pipeline + road_gravel_1l + trail + conventional seismic + vegetated_edge_roads
+
+# H8: Baseline world without humans, coyotes ~ water + shrub + grass + conifer + broadleaf + mixed_forest
+
+# Okay, so are we violating any assumptions? Only test one before, rest at the end.
+
+# Assumption 1 - model variables are independent
+chart.Correlation(coyote_data[c("pipeline", "road_gravel_1l", "trail", "conventional_seismic", "vegetated_edge_roads", "wolf_tot_det", "deer_tot_det", "moose_tot_det", "coy_tot_det", "hare_tot_det", "water", "shrub", "grass", "conifer", "broadleaf", "mixed_forest")], 
+                  histogram = TRUE, 
+                  method = "spearman")
+### Result: none of the covariates are highly correlated, aka r^2 > 0.7
+
+# GLM TIME
+
+# H0: Null
+glm_H0 <- glm(coy_tot_det ~ 1,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H0)
+
+# H1: Coyotes like wide features, coyotes ~ pipeline + road_gravel_1l
+glm_H1 <- glm(coy_tot_det ~ pipeline + road_gravel_1l,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H1)
+
+# H2: Coyote use of wide features depends on the presence of wolf competitors, coyote ~ pipeline + road_gravel_1l + pipeline:wolf + road_gravel_1l:wolf
+glm_H2 <- glm(coy_tot_det ~ pipeline + road_gravel_1l + pipeline:wolf_tot_det + road_gravel_1l:wolf_tot_det,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H2)
+
+# H3: Coyote use of wide features varies with the presence of prey, coyote ~ pipeline + road_gravel_1l + deer_tot_det + hare_tot_det + moose_tot_det
+glm_H3 <- glm(coy_tot_det ~ pipeline + road_gravel_1l + deer_tot_det + hare_tot_det + moose_tot_det,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H3)
+
+# H4: Coyotes like narrow features, coyote ~ trail + conventional_seismic + vegetated_edge_roads
+glm_H4 <- glm(coy_tot_det ~ trail + conventional_seismic + vegetated_edge_roads,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H4)
+
+# H5: Coyote use of narrow features depends on presence of competitors, coyote ~ trail + conventional_seismic + vegetated_edge_roads + trail:wolf_tot_det + conventional_seismic:wolf_tot_det + vegetated_edge_roads:wolf_tot_det
+glm_H5 <- glm(coy_tot_det ~ trail + conventional_seismic + vegetated_edge_roads + trail:wolf_tot_det + conventional_seismic:wolf_tot_det + vegetated_edge_roads:wolf_tot_det,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H5)
+
+# H6: Coyote use of narrow features also varies with the presence of prey, coyotes ~ trail + conventional_seismic + vegetated_edge_roads + deer + hare + moose
+glm_H6 <- glm(coy_tot_det ~ trail + conventional_seismic + vegetated_edge_roads + deer_tot_det + hare_tot_det + moose_tot_det,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H6)
+
+# H7: Coyotes like both wide and narrow features, coyote ~ pipeline + road_gravel_1l + trail + conventional seismic + vegetated_edge_roads
+glm_H7 <- glm(coy_tot_det ~ pipeline + road_gravel_1l + trail + conventional_seismic + vegetated_edge_roads,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H7)
+
+# H8: Baseline world without humans, coyotes ~ water + shrub + grass + conifer + broadleaf + mixed_forest
+glm_H8 <- glm(coy_tot_det ~ water + shrub + grass + conifer + broadleaf + mixed_forest,
+              data = coyote_data,
+              family = poisson)
+
+summary(glm_H8)
 
 
+# GLM assumptions continued 
+
+# Assumption 2 - homogeneity of variance using Levene's test
+# Specify the variables you want to test
+vars_to_test <- c("pipeline", "road_gravel_1l", "trail", "conventional_seismic", "vegetated_edge_roads", "wolf_tot_det", "deer_tot_det", "moose_tot_det", "coy_tot_det", "hare_tot_det", "water", "shrub", "grass", "conifer", "broadleaf", "mixed_forest")
+
+# Perform Levene's test for homogeneity of variances
+levene_results <- leveneTest(as.formula(paste0("~", paste(vars_to_test, collapse = "+"))), data = coyote_data)
+
+## I get an error that says "Levene's test is not appropriate for quantitative explanatory variables." I don't think that we have categorical variables or groups, so moving on
+
+# Assumption 3 - Normality of residuals 
+
+# Calculate chi square for residual deviance for each glm
+# H0
+summary(glm_H0)
+# = residual deviance / degrees of freedom
+1593.5/154
+# = 10, so overdispersed
+
+# H1 
+summary(glm_H1)
+# = residual deviance / degrees of freedom
+1386.9/149
+# = 9.3081, so overdispersed 
+
+# H2
+summary(glm_H2)
+# = residual deviance / degrees of freedom
+1356.3/147
+# = 9.2265 
+
+#####****-----CONTINUE HERE! Testing the residual deviance and interpreting. Marissa mentions if it is overdispersed to use the negative binomial distribution in the modelling. https://marissadyck.github.io/ES482-R-labs.github.io/mod_2_glm.html
+
+# Assumption 4 - Influential observations. **TBC
