1b57157e422cb678d92680053ff798155f1b6c22
jfclarke <74422143+jfclarke@users.noreply.github.com>
2024-04-07 17:29:11 -0700

    Add files via upload

--- coyote_glm.R
+++ coyote_glm.R
@@ -1,10 +1,7 @@
 # ES582 - Coyote GLM
 # Jamie Clarke
 
-### NEED ABSENCES AS WELL AS PRESENCES!!
-### NEED PROPORTIONAL DETS OF SNOWSHOE HARES
-
-# last updated Apr 5
+# last updated Apr 7
 
 library(cowplot)
 library(tidyverse)
@@ -18,6 +15,43 @@ library(lme4)
 library(rphylopic)
 library(MuMIn)
 library(AER)
+library(broom)
+
+
+# STEP 0 - DATA VIS -------------------------------------------------------
+
+# plot frequency for LFs
+# lf_vis = visualizing LFs!
+lf_vis <- read.csv('data/OSM_2022_covariates.csv') %>% 
+  filter(buff_dist == 1000)
+
+hist(lf_vis$pipeline) # in
+hist(lf_vis$truck_trail) # out
+hist(lf_vis$vegetated_edge_roads) # could use this...
+hist(lf_vis$road_paved_1l) # out
+hist(lf_vis$conventional_seismic) # in
+hist(lf_vis$road_gravel_1l) # in
+hist(lf_vis$road_paved_undiv_1l) # out
+hist(lf_vis$road_gravel_2l) # out
+hist(lf_vis$trail) # in
+hist(lf_vis$road_unclassified) # out
+hist(lf_vis$low_impact_seismic) # in
+hist(lf_vis$road_paved_undiv_2l) # out
+hist(lf_vis$road_unimproved) # out
+hist(lf_vis$vegetated_edge_railways) # out
+hist(lf_vis$transmission_line) # in
+hist(lf_vis$road_unpaved_2l) # out
+hist(lf_vis$road_paved_3l) # out
+hist(lf_vis$road_paved_div) # out
+hist(lf_vis$road_unpaved_1l) # out
+hist(lf_vis$road_paved_5l) # out
+hist(lf_vis$road_paved_4l) # out
+
+# SUMMARY: wide features = gravel roads + pipelines, narrow features = trails, seismic lines + unimproved roads
+
+# NEW THOUGHTS - confirming we want to use unimproved roads - justification?
+#                in this version, I also combine seismic line types... makes sense to me, but any thoughts/feelings/concerns?
+
 
 # STEP 1 - COMBINE DATA INTO SINGLE FILE -------------------------------------------------
 
@@ -25,7 +59,7 @@ library(AER)
 # lf_covs = LF-related covariates
 lf_covs <- read.csv('data/OSM_2022_covariates.csv') %>% 
   
-  select(site, buff_dist, trail, conventional_seismic, pipeline, transmission_line, road_unimproved, road_gravel_1l, road_gravel_2l, truck_trail, road_paved_undiv_2l, low_impact_seismic, road_paved_1l, lc_class20, lc_class50, lc_class110, lc_class210, lc_class220, lc_class230) %>% 
+  select(site, buff_dist, trail, conventional_seismic, low_impact_seismic, pipeline, transmission_line, road_gravel_1l, road_gravel_2l, road_unimproved, lc_class20, lc_class50, lc_class110, lc_class210, lc_class220, lc_class230) %>% 
   
   rename(water = lc_class20,
          shrub = lc_class50,
@@ -33,6 +67,10 @@ lf_covs <- read.csv('data/OSM_2022_covariates.csv') %>%
          broadleaf = lc_class220,
          mixed_forest = lc_class230,
          conifer = lc_class210) %>% 
+  
+  mutate(gravel_roads = road_gravel_1l + road_gravel_2l) %>% 
+  
+  mutate(seismic_lines = conventional_seismic + low_impact_seismic) %>% 
   
   na.omit()
 
@@ -68,30 +106,37 @@ coyote_data <- coyote_det %>%
   
   filter(buff_dist == 1000) %>% 
   
-  na.omit() %>% 
-  
-  mutate_if(is.numeric, scale)
-
+  na.omit()
 
 # STEP 2 = GLM ----------------------------------------------------------------------
 
-# check for autocorrelation
+# first: check for autocorrelation
 
-chart.Correlation(coyote_data[c('trail', 'conventional_seismic', 'pipeline', 'transmission_line', 'road_unimproved', 'road_gravel_1l', 'road_gravel_2l', 'truck_trail', 'road_paved_undiv_2l', 'low_impact_seismic', 'road_paved_1l', 'snowshoe_hare', 'unknown_deer', 'white_tailed_deer', 'grey_wolf', 'moose')], 
-                  histogram = TRUE, 
-                  method = "pearson")
+coyote_cor <- coyote_data %>%
+  
+  select(trail, seismic_lines, pipeline, transmission_line, road_unimproved, gravel_roads, snowshoe_hare, unknown_deer, white_tailed_deer, grey_wolf, moose, water, shrub, grass, broadleaf, conifer, mixed_forest)
 
+summary(coyote_cor)
 
-chart.Correlation(coyote_data[c('trail', 'conventional_seismic', 'pipeline', 'transmission_line', 'road_unimproved', 'road_gravel_1l', 'road_gravel_2l', 'truck_trail', 'road_paved_undiv_2l', 'low_impact_seismic', 'road_paved_1l', 'snowshoe_hare', 'unknown_deer', 'white_tailed_deer', 'grey_wolf', 'moose')], 
-                  histogram = TRUE, 
-                  method = "spearman")
+chart.Correlation(coyote_cor,
+                  histogram = TRUE,
+                  method = 'pearson')
 
-# LEARNED FROM CORRELATION PLOTS: 1) unknown_deer and white_tailed_deer are autocorrelated (0.7 or 0.73), so get rid of unknown_deer (checked w/ Marissa)
-#                                 2) pipeline and transmission_line are autocorrelated (0.77 or 0.63), so get rid of transmission line (?)
-#                                 3) there are no detections on road_paved_1l, so don't need!
-#                                 4) Pearson's and Spearman give similar results!
+chart.Correlation(coyote_cor,
+                  histogram = TRUE,
+                  method = 'spearman')
 
-# testing candidate models
+# LEARNED FROM CORRELATION PLOTS: 1) unknown_deer and white_tailed_deer are autocorrelated (0.7 or 0.73), so get rid of unknown_deer
+#                                 2) pipeline and transmission_line are autocorrelated (0.77 or 0.63)
+#                                 3) broadleaf and conifer are autocorrelated (0.78 and 0.66) AND broadleaf and WTD are autocorrelated (0.54 and 0.63), this seems legit so not worried about it??
+#                                 4) pipeline and gravel_roads are pretty autocorrelated, esp according to Spearman
+#                                 5) Pearson's and Spearman give similar results!
+
+# SUMMARY: wide features = transmission lines + gravel roads, narrow features = trails, seismic lines + unimproved roads
+
+# NEW THOUGHTS: should we group transmission lines + piplelines = above-ground LF disturbances and see if not autocorrelated w/ gravel roads? I know there wasn't huge love for this idea last week but worth revisiting?
+
+# next: test candidate models using proportional binomial GLMs
 
 # null hypothesis
 H0 <- glm(
@@ -102,106 +147,85 @@ H0 <- glm(
 # coyotes like wide features
 H1 <- glm(
           cbind(coyote, absent_coyote) ~
-            pipeline +
-            road_unimproved +
-            road_gravel_1l +
-            road_gravel_2l +
-            truck_trail +
-            road_paved_undiv_2l,
+            scale(transmission_line) +
+            scale(gravel_roads),
           data = coyote_data,
           family = binomial)
 
 # coyote use of wide features depends on presence of competitors
 H2 <- glm(
   cbind(coyote, absent_coyote) ~
-    pipeline +
-    road_unimproved +
-    road_gravel_1l +
-    road_gravel_2l +
-    truck_trail +
-    road_paved_undiv_2l +
-    pipeline:grey_wolf +
-    transmission_line:grey_wolf +
-    road_unimproved:grey_wolf +
-    road_gravel_1l:grey_wolf +
-    road_gravel_2l:grey_wolf +
-    truck_trail:grey_wolf +
-    road_paved_undiv_2l:grey_wolf,
+    scale(transmission_line) +
+    scale(gravel_roads) +
+    scale(transmission_line):scale(grey_wolf) +
+    scale(gravel_roads):scale(grey_wolf),
   data = coyote_data,
   family = binomial)
 
 # coyotes use of wide features also varies with presence of prey
 H3 <- glm(
   cbind(coyote, absent_coyote) ~
-    pipeline +
-    road_unimproved +
-    road_gravel_1l +
-    road_gravel_2l +
-    truck_trail +
-    road_paved_undiv_2l +
-    snowshoe_hare +
-    white_tailed_deer +
-    moose,
+    scale(transmission_line) +
+    scale(gravel_roads) +
+    scale(snowshoe_hare) +
+    scale(white_tailed_deer) +
+    scale(moose),
   data = coyote_data,
   family = binomial)
 
 # coyotes like narrow features
 H4 <- glm(
   cbind(coyote, absent_coyote) ~
-    trail +
-    conventional_seismic +
-    low_impact_seismic,
+    scale(trail) +
+    scale(seismic_lines) +
+    scale(road_unimproved),
   data = coyote_data,
   family = binomial)
 
 # coyote use of narrow features depends on presence of competitors
-H5 <- H4 <- glm(
+H5 <- glm(
   cbind(coyote, absent_coyote) ~
-    trail +
-    conventional_seismic +
-    low_impact_seismic +
-    trail:grey_wolf +
-    conventional_seismic:grey_wolf +
-    low_impact_seismic:grey_wolf,
+    scale(trail) +
+    scale(seismic_lines) +
+    scale(road_unimproved) +
+    scale(trail):scale(grey_wolf) +
+    scale(seismic_lines):scale(grey_wolf) +
+    scale(road_unimproved):scale(grey_wolf),
   data = coyote_data,
   family = binomial)
 
 # coyote use of narrow features also varies with presence of prey
 H6 <- glm(
   cbind(coyote, absent_coyote) ~
-    trail +
-    conventional_seismic +
-    low_impact_seismic +
-    snowshoe_hare +
-    white_tailed_deer +
-    moose,
+    scale(trail) +
+    scale(seismic_lines) +
+    scale(road_unimproved) +
+    scale(snowshoe_hare) +
+    scale(white_tailed_deer) +
+    scale(moose),
   data = coyote_data,
   family = binomial)
 
 # coyotes like wide and narrow features
 H7 <- glm(
   cbind(coyote, absent_coyote) ~
-    pipeline +
-    road_unimproved +
-    road_gravel_1l +
-    road_gravel_2l +
-    truck_trail +
-    road_paved_undiv_2l +
-    trail +
-    conventional_seismic +
-    low_impact_seismic,
+    scale(transmission_line) +
+    scale(gravel_roads) +
+    scale(trail) +
+    scale(seismic_lines) +
+    scale(road_unimproved),
   data = coyote_data,
   family = binomial)
 
 # coyotes like natural features
 H8 <- glm(
   cbind(coyote, absent_coyote) ~
-    water +
-    shrub +
-    grass +
-    conifer +
-    broadleaf +
-    mixed_forest,
+    scale(water) +
+    scale(shrub) +
+    scale(grass) +
+    scale(conifer) +
+    scale(broadleaf) +
+    scale(mixed_forest),
   data = coyote_data,
   family = binomial)
 
@@ -212,26 +236,209 @@ H8 <- glm(
 
 # test for residual deviance - proportional binomial does NOT assume even dispersion so ALL GOOD DAWG
 
-
 # STEP 4 - MODEL SELECTION -------------------------------------------
 
 model.sel(H0, H1, H2, H3, H4, H5, H6, H7, H8)
 
-# model 3 = best-performing, by more than 2 AICc!
-# so: coyotes use of wide features also varies with presence of prey!
-
-# model 7 = second-best performing
-# null model = third-best performing
+# model 3 (wide features + prey) = best-performing, BUT not by 2 AICc
+# model 8 (natural features) = second best
 
 # STEP 5 - INTERPRETING MODEL OUTPUTS ------------------------------------------------
 
-# odds ratios
-# interpretation: below 1 = negative association, above 1 = positive association
-#                 value of the exponentiated coefficient = change in odds of the response outcome given 1 unit increase in the explanatory variable
+# odds ratios for H3
+H3_odds <- tidy(H3,
+       exponentiate = TRUE,
+       confint.int = TRUE) %>% 
+  
+  # bind estimates + confidence intervals from model
+  cbind(exp(confint(H3))) %>% 
+  
+  # change format to a tibble so works nicely with ggplot
+  as_tibble() %>% 
+  
+  rename(lower = '2.5 %',
+         upper = '97.5 %') %>% 
+  
+  filter(term != '(Intercept)')
 
-exp(coefficients(H3))
+# plot odds ratios for H3
+plot_1 <- ggplot(data = H3_odds,aes(x = term, y = estimate)) +
+    
+    # add points for odds
+    geom_point() +
+  
+  # add error bars = confidence intervals
+  geom_errorbar(aes(ymin = lower,
+                    ymax = upper),
+                linewidth = 0.5,
+                width = 0.4) +
+  
+  geom_hline(yintercept = 1,
+             alpha = 0.5) +
+  
+  # rename the x axis labels
+  scale_x_discrete(labels = c('transmission line',
+                              'gravel roads',
+                              'snowshoe hare',
+                              'white-tailed deer',
+                              'moose')) +
+  
+  # axis titles
+  ylab('odds ratio') +
+  
+  # flip x and y axis 
+  coord_flip() +
+  
+  # specify theme
+  theme_bw() +
+  
+  # specify theme elements
+  theme(panel.grid = element_blank(),
+        axis.title.y = element_blank())
 
-summary(H3)
+plot_1 # shows that gravel roads have negative effect, moose + snowshoe hare detections and transmission lines have positive effect on coyote occurrence; white-tailed deer detections have a smaller impact (cross over 1 line)
 
-hist(coyote_data$pipeline)
-hist(coyote_data$truck_trail)
+# odds ratio for H8
+H8_odds <- tidy(H8,
+                exponentiate = TRUE,
+                confint.int = TRUE) %>% 
+  
+  # bind estimates + confidence intervals from model
+  cbind(exp(confint(H8))) %>% 
+  
+  # change format to a tibble so works nicely with ggplot
+  as_tibble() %>% 
+  
+  rename(lower = '2.5 %',
+         upper = '97.5 %') %>% 
+  
+  filter(term != '(Intercept)')
+
+# plot odds ratios for H8
+plot_2 <- ggplot(data = H8_odds,aes(x = term, y = estimate)) +
+  
+  # add points for odds
+  geom_point() +
+  
+  # add error bars = confidence intervals
+  geom_errorbar(aes(ymin = lower,
+                    ymax = upper),
+                linewidth = 0.5,
+                width = 0.4) +
+  
+  geom_hline(yintercept = 1,
+             alpha = 0.5) +
+  
+  # rename the x axis labels
+  scale_x_discrete(labels = c('wataer',
+                              'shrubs',
+                              'grass',
+                              'coniferous forest',
+                              'broadleaf forest',
+                              'mixed forest')) +
+  
+  # axis titles
+  ylab('odds ratio') +
+  
+  # flip x and y axis 
+  coord_flip() +
+  
+  # specify theme
+  theme_bw() +
+  
+  # specify theme elements
+  theme(panel.grid = element_blank(),
+        axis.title.y = element_blank())
+
+plot_2 # what the heck?? this seems wrong - coyote occurence negatively influenced by all natural features...??
+
+# DOES IT MAKE MORE SENSE TO LUMP FOREST TYPES TOGETHER, GIVEN BROADLEAF + CONIFER ARE AUTOCORRELATED?
+
+
+# TEST GROUPING TREE TYPES ---------------------------------------------------
+
+group_trees <- coyote_data %>% 
+  
+  mutate(forest = conifer + broadleaf + mixed_forest)
+
+# check for autocorrelation
+coyote_trees_cor <- group_trees %>%
+  
+  select(trail, seismic_lines, transmission_line, road_unimproved, gravel_roads, snowshoe_hare, white_tailed_deer, grey_wolf, moose, water, shrub, grass, forest)
+
+chart.Correlation(coyote_trees_cor,
+                  histogram = TRUE,
+                  method = 'pearson')
+
+chart.Correlation(coyote_trees_cor,
+                  histogram = TRUE,
+                  method = 'spearman')
+
+# SUMMARY: seems better, shrubs + forest now autocorrelated for Pearson (0.64) but not for Spearman (0.59)...
+
+# try running GLM for H8 again
+H8_trees <- glm(
+  cbind(coyote, absent_coyote) ~
+    scale(water) +
+    scale(shrub) +
+    scale(grass) +
+    scale(forest),
+  data = group_trees,
+  family = binomial)
+
+# updated model selection
+model.sel(H0, H1, H2, H3, H4, H5, H6, H7, H8_trees) # H8 still comes out as second-best model
+
+# odds ratio for H8_trees
+H8_trees_odds <- tidy(H8_trees,
+                exponentiate = TRUE,
+                confint.int = TRUE) %>% 
+  
+  # bind estimates + confidence intervals from model
+  cbind(exp(confint(H8_trees))) %>% 
+  
+  # change format to a tibble so works nicely with ggplot
+  as_tibble() %>% 
+  
+  rename(lower = '2.5 %',
+         upper = '97.5 %') %>% 
+  
+  filter(term != '(Intercept)')
+
+# plot odds ratios for H8
+plot_3 <- ggplot(data = H8_trees_odds,aes(x = term, y = estimate)) +
+  
+  # add points for odds
+  geom_point() +
+  
+  # add error bars = confidence intervals
+  geom_errorbar(aes(ymin = lower,
+                    ymax = upper),
+                linewidth = 0.5,
+                width = 0.4) +
+  
+  geom_hline(yintercept = 1,
+             alpha = 0.5) +
+  
+  # rename the x axis labels
+  scale_x_discrete(labels = c('wataer',
+                              'shrubs',
+                              'grass',
+                              'coniferous forest',
+                              'broadleaf forest',
+                              'mixed forest')) +
+  
+  # axis titles
+  ylab('odds ratio') +
+  
+  # flip x and y axis 
+  coord_flip() +
+  
+  # specify theme
+  theme_bw() +
+  
+  # specify theme elements
+  theme(panel.grid = element_blank(),
+        axis.title.y = element_blank())
+
+plot_3 # still fucked...
